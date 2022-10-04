c712a2b993ed532d3d9e591b00b737211638b3e5
mkreive <mkreive@gmail.com>
2022-10-04 15:53:06 +0300

    x forward done

--- script.js
+++ script.js
@@ -2,8 +2,3 @@
 
 // CHALLENGE-2
 const menuBtn = document.querySelector('.menu__button');
-
-menuBtn.addEventListener('click', function () {
-    console.log('clicked');
-    menuBtn.classList.toggle('clicked');
-});
