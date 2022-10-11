75fabaa85fd753138401f176088955549370a529
mkreive <mkreive@gmail.com>
2022-10-11 09:38:56 +0300

    hidden and shift classes added

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 'use strict';
 
-// CHALLENGE-2
-const menuBtn = document.querySelector('.menu__button');
+// CHALLENGE-4
+const menuBurger = document.querySelector('.menu-burger__2');
+const hiddenNav = document.querySelector('.card_blue');
+const movingCard = document.getElementById('moving-card');
+
+menuBurger.addEventListener('click', function () {
+    console.log('clicked');
+    hiddenNav.classList.toggle('hidden');
+    movingCard.classList.toggle('shift');
+});
